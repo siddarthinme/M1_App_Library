dc10455f5b4da32431c685cd178b47f48e6d7791
siddarthinme <94165024+siddarthinme@users.noreply.github.com>
2021-11-24 23:49:08 +0530

    Add files via upload

---
+++ 3_Implementation/src/library.c
@@ -0,0 +1,7 @@
+#include "library.h"
+int div(int num1, int num2)
+{
+    if(num2 == 0)
+        return 0;
+    return num1/num2;
+}
---
+++ 3_Implementation/src/library.h
@@ -0,0 +1,24 @@
+/**
+ * @file library.h
+ * @author Siddarood
+ * @brief Library Management System
+ * @version 0.1
+ * @date 2021-11-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __LIBRARY_H__
+#define __LIBRARY_H__
+#include<stdio.h>
+#include<assert.h>
+/**
+ * @brief 
+ * 
+ * @param num1 
+ * @param num2 
+ * @return int 
+ */
+int div(int num1, int num2);
+#endif 
